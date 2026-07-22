21823a8aaae2865f1894090ea03adfa0a52e0b55
Philippe Belmont Guerrón <PBG-Ec@users.noreply.github.com>
2026-07-22 12:50:00 +0200

    Update composition table for Ecuador with new data

--- CEDIA_reformCompTable.R
+++ CEDIA_reformCompTable.R
@@ -37,12 +37,12 @@ tc$fuente_[is.na(tc$fuente_)] <- 3 # Original Ec table
 
 ### Build dbs Update : SrLeg (sr), Rework Tc_ec (t4), Original Tc_ec (tc)
 # Update: Sr Legacy 
-tc_sr <- tc[tc$fuente_==1,c("id_srleg","fuente_","IngredientesCodigo")]
+tc_sr <- tc[tc$fuente_==1,c("id_srleg","fuente_","IngredientesCodigo","alim")]
 tc_sr <- merge(tc_sr,sr,by.x="id_srleg",by.y="food_id",all.x=T)
 
 # Update V4 Tcomp  
-tc_t4 <- tc[tc$fuente_==2,c("cd_t4","fuente_","IngredientesCodigo","id_srleg")]
-tc_t4 <- merge(tc_t4,t4[,-grep("fuente",names(t4))],by="cd_t4", all.x=T)
+tc_t4 <- tc[tc$fuente_==2,c("cd_t4","fuente_","IngredientesCodigo","id_srleg","alim")]
+tc_t4 <- merge(tc_t4,t4[,-grep("fuente|alim",names(t4))],by="cd_t4", all.x=T)
 for(i in names(tc_t4)[names(tc_t4) %in% rnvars$v_tec]){
   nnam <- rnvars$v_sr[which(rnvars$v_tec==i)]
   ifelse(is.na(nnam),nnam <- i, nnam <- nnam)
